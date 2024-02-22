5f62cf770efea251762bc87aeedb1668a6e80fbb
Y39WebDeveloper <y39freelancer@gmail.com>
2024-02-22 11:48:41 +0100

    .

---
+++ README.md
@@ -0,0 +1,1 @@
+"# live : https://y39webdeveloper.github.io/cazy/" 
---
+++ js/script.js
@@ -0,0 +1,27 @@
+let menuBtn = document.querySelector('#menuBtn');
+let menu = document.querySelector('.navbar .nav ul');
+let layer = document.querySelector('.layer');
+let counter = document.querySelector('#counter');
+let plus = document.querySelector('#plus');
+let moin = document.querySelector('#moin');
+
+plus.addEventListener('click', plusCounter);
+moin.addEventListener('click', moinCounter);
+function plusCounter(){
+    counter.innerHTML < 30 ? counter.innerHTML++ : '';
+}
+function moinCounter(){
+    counter.innerHTML > 0 ? counter.innerHTML-- : '';
+}
+
+menuBtn.addEventListener('click', showMenu);
+layer.addEventListener('click', showMenu);
+function showMenu(){
+    if(!menu.classList.contains('active')){
+        menu.classList.add('active');
+        layer.classList.add('active');
+    }else{
+        menu.classList.remove('active');
+        layer.classList.remove('active');
+    }
+}
